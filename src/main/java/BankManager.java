6fd3afb3a9a7f3100b6226db8d71d345592fa565
bkhaled <Bilel.KHALED.ext@eulerhermes.com>
2018-12-13 20:59:43 +0100

    US 2

--- src/main/java/BankManager.java
+++ src/main/java/BankManager.java
@@ -2,12 +2,19 @@
 public class BankManager {
 	public static void main(String[] args) {
 		Account account = new Account("bilel", 01, 2000);
+		System.out.println(" ######### Deposit #######");
 
 		account.deposit(15);
-	
 		System.out.println("Account owner: " + account.getName());
 		System.out.printf("Balance: €%.2f\n", account.getBalance());
 	
+	
+		
+		System.out.println(" ######### Deposit #######");
+
+				account.withdraw(50);
+				System.out.println("Account owner: " + account.getName());
+				System.out.printf("Balance: €%.2f\n", account.getBalance());
 		
 	}
 }
--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -13,5 +13,13 @@ public class AccountTest {
 assertEquals(2014, account.getBalance(),0);
 
 	}
+	
+	@Test
+	public void bank_manager_should_reduce_balance_when_withdraw() throws Exception {
+		Account account = new Account("bilel", 01, 2000);
+		account.withdraw(50);
+assertEquals(1950, account.getBalance(),0);
+
+	}
 
 }
